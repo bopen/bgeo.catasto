c110ed6b3911c0b54915a8d2f71ab76f006f3d80
Alessandro Amici <a.amici@bopen.it>
2011-07-25 15:30:03 +0200

    new: consolidate parsing new: start saving all data in a dictionary

--- bgeo/catasto/__init__.py
+++ bgeo/catasto/__init__.py
@@ -12,34 +12,47 @@ gauss_boaga_ovest.ImportFromEPSG(3004)
 
 trasformation = CoordinateTransformation(local_cassini_soldener, gauss_boaga_ovest)
 
+def isole(cxf, oggetto):
+    for isola in range(int(oggetto['NUMEROISOLE'])):
+        cxf.next().strip()
+
+def vertici(cxf, oggetto):
+    for vertice in range(int(oggetto['NUMEROVERTICI'])):
+        cxf.next().strip(), cxf.next().strip()
 
 oggetti_cartografici = {
-    'BORDO': ['CODICE IDENTIFICATIVO', 'DIMENSIONE', 'ANGOLO',
+    'BORDO': (['CODICE IDENTIFICATIVO', 'DIMENSIONE', 'ANGOLO',
         'POSIZIONEX', 'POSIZIONEY', 'PUNTOINTERNOX', 'PUNTOINTERNOY',
-        'NUMEROISOLE', 'NUMEROVERTICI', isole, vertici],
-    'TESTO': ['TESTO', 'DIMENSIONE', 'ANGOLO','POSIZIONEX', 'POSIZIONEY'],
+        'NUMEROISOLE', 'NUMEROVERTICI'], [isole, vertici]),
+    'TESTO': (['TESTO', 'DIMENSIONE', 'ANGOLO','POSIZIONEX', 'POSIZIONEY'], []),
+    'FIDUCIALE': (['NUMERO IDENTIFICATIVO', 'CODICE SIMBOLO', 'POSIZIONEX', 'POSIZIONEY',
+        'PUNTORAPPRESENTAZIONEX', 'PUNTORAPPRESENTAZIONEY'], []),
+    'SIMBOLO': (['CODICE SIMBOLO', 'ANGOLO', 'POSIZIONEX', 'POSIZIONEY'], []),
+    'LINEA': (['CODICE TIPO DI TRATTO', 'NUMEROVERTICI'], [vertici]),
+    'EOF': ([], []),
 }
-['BORDO', 'TESTO', 'SIMBOLO', 'FIDUCIALE', 'LINEA', 'EOF']
 
 
 def do_main(basepath):
+    foglio = {}
+
     basename = path_basename(basepath).upper()
     
     # check basename
     assert len(basename) == 11
     
-    codice_comune = basename[:4]
-    codice_sezione_censuaria = basename[4]
-    assert codice_sezione_censuaria in ['A', 'B', '_']
+    foglio['CODICE COMUNE'] = basename[:4]
+    foglio['CODICE SEZIONE CENSUARIA'] = basename[4]
+    assert foglio['CODICE SEZIONE CENSUARIA'] in ['A', 'B', '_']
     
-    codice_numero_foglio = basename[5:9]
-    numero_foglio = codice_numero_foglio.lstrip('0')
+    foglio['CODICE NUMERO FOGLIO'] = basename[5:9]
+    foglio['NUMERO FOGLIO'] = foglio['CODICE NUMERO FOGLIO'].lstrip('0')
 
-    codice_allegato = basename[9]
-    assert codice_allegato in ['0', 'Q'] # missing
+    foglio['CODICE ALLEGATO'] = basename[9]
+    assert foglio['CODICE ALLEGATO'] in ['0', 'Q'] # missing
 
-    codice_sviluppo = basename[10]
-    assert codice_sviluppo in ['0', 'U']
+    foglio['CODICE SVILUPPO'] = basename[10]
+    assert foglio['CODICE SVILUPPO'] in ['0', 'U']
     
     cxf = open(basepath + '.CXF')
     sup = open(basepath + '.SUP')
@@ -55,54 +68,37 @@ def do_main(basepath):
 
     for raw_line in cxf:
         line = raw_line.strip().rstrip('\\')
-        assert line in oggetti_cartografici.keys(), line
-        
-        if line == 'BORDO':
-            elemento = {}
-            for nome_record in ['CODICE IDENTIFICATIVO', 'DIMENSIONE', 'ANGOLO', 'POSIZIONEX', 'POSIZIONEY',
-                    'PUNTOINTERNOX', 'PUNTOINTERNOY', 'NUMEROISOLE', 'NUMEROVERTICI']:
-                elemento[nome_record] = cxf.next().strip()
-            if len(elemento['CODICE IDENTIFICATIVO']) == 11:
-                print '*** CONFINE', elemento['CODICE IDENTIFICATIVO']
-            elif elemento['CODICE IDENTIFICATIVO'] == 'STRADA':
-                print '*** STRADA', elemento['CODICE IDENTIFICATIVO']
-            elif elemento['CODICE IDENTIFICATIVO'] == 'ACQUA':
-                print '*** FABBRICATO', elemento['CODICE IDENTIFICATIVO']
-            elif elemento['CODICE IDENTIFICATIVO'].endswith('+'):
-                print '*** FABBRICATO in PARTICELLA', elemento['CODICE IDENTIFICATIVO'][:-1]
-            else:
-                print '*** PARTICELLA', elemento['CODICE IDENTIFICATIVO']
-            for isola in range(int(elemento['NUMEROISOLE'])):
-                cxf.next().strip()
-            for vertice in range(int(elemento['NUMEROVERTICI'])):
-                cxf.next().strip(), cxf.next().strip()
-        elif line == 'FIDUCIALE':
-            elemento = {}
-            for nome_record in ['NUMERO IDENTIFICATIVO', 'CODICE SIMBOLO', 'POSIZIONEX', 'POSIZIONEY',
-                    'PUNTORAPPRESENTAZIONEX', 'PUNTORAPPRESENTAZIONEY']:
-                elemento[nome_record] = cxf.next().strip()
-            print 'FIDUCIALE', 'PF%02d/%s%s/%s' % (int(elemento['NUMERO IDENTIFICATIVO']), codice_numero_foglio[1:], codice_allegato, codice_comune)
-            x,  y = float(elemento['POSIZIONEX']), float(elemento['POSIZIONEY'])
-            print x, y, trasformation.TransformPoint(x, y)[:2]
-        elif line == 'LINEA':
-            elemento = {}
-            for nome_record in ['CODICE TIPO DI TRATTO', 'NUMEROVERTICI']:
-                elemento[nome_record] = cxf.next().strip()
-            for vertice in range(int(elemento['NUMEROVERTICI'])):
-                cxf.next().strip(), cxf.next().strip()
-        elif line == 'SIMBOLO':
-            elemento = {}
-            for nome_record in ['CODICE SIMBOLO', 'ANGOLO', 'POSIZIONEX', 'POSIZIONEY']:
-                elemento[nome_record] = cxf.next().strip()
-        elif line == 'TESTO':
-            elemento = {}
-            for nome_record in ['TESTO', 'DIMENSIONE', 'ANGOLO','POSIZIONEX', 'POSIZIONEY']:
-                elemento[nome_record] = cxf.next().strip()
-        elif line == 'EOF':
+        assert line in oggetti_cartografici, 'Unknown object %r' % line
+
+        oggetto = {}
+        nomi_record, parse_functions = oggetti_cartografici[line]
+        for nome_record in nomi_record:
+            oggetto[nome_record] = cxf.next().strip()
+
+        for parse_function in parse_functions:
+            parse_function(cxf, oggetto)
+
+#        if len(oggetto['CODICE IDENTIFICATIVO']) == 11:
+#            print '*** CONFINE', elemento['CODICE IDENTIFICATIVO']
+#        elif oggetto['CODICE IDENTIFICATIVO'] == 'STRADA':
+#            print '*** STRADA', elemento['CODICE IDENTIFICATIVO']
+#        elif oggetto['CODICE IDENTIFICATIVO'] == 'ACQUA':
+#            print '*** FABBRICATO', elemento['CODICE IDENTIFICATIVO']
+#        elif oggetto['CODICE IDENTIFICATIVO'].endswith('+'):
+#            print '*** FABBRICATO in PARTICELLA', elemento['CODICE IDENTIFICATIVO'][:-1]
+#        else:
+#            print '*** PARTICELLA', elemento['CODICE IDENTIFICATIVO']
+
+
+#            print 'FIDUCIALE', 'PF%02d/%s%s/%s' % (int(elemento['NUMERO IDENTIFICATIVO']), codice_numero_foglio[1:], codice_allegato, codice_comune)
+#            x,  y = float(elemento['POSIZIONEX']), float(elemento['POSIZIONEY'])
+#            print x, y, trasformation.TransformPoint(x, y)[:2]
+        if line == 'EOF':
             # record di terminazione
             break
-    
-    assert cxf.readline() == ''
+
+    garbage = cxf.readline()
+    assert garbage == '', 'Garbage after EOF %r' % garbage
     
 
 def main():
