2751b77cf12a4b888bdef5ec6fa4599c9156a38c
Alessandro Amici <a.amici@bopen.it>
2011-08-09 14:53:15 +0200

    new: add TESTI and LINEE layers. All data in CXF is exported

--- bgeo/catasto/ogr.py
+++ bgeo/catasto/ogr.py
@@ -4,7 +4,8 @@ from os.path import join, basename as path_basename, splitext
 import sys
 
 from osgeo.osr import CoordinateTransformation, SpatialReference
-from osgeo.ogr import GetDriverByName, wkbPoint, wkbPolygon, wkbLinearRing, Feature, Geometry, FieldDefn
+from osgeo.ogr import wkbPoint, wkbPolygon, wkbLinearRing, wkbLineString
+from osgeo.ogr import GetDriverByName, Feature, Geometry, FieldDefn
 from osgeo.ogr import OFTString, OFTInteger, OFTReal
 
 local_cassini_soldener = SpatialReference()
@@ -30,6 +31,8 @@ def write_foglio(foglio, destination, format_name='ESRI Shapefile'):
     f_tipo.SetWidth(11)
     f_part = FieldDefn('PARTICELLA', OFTString)
     f_part.SetWidth(8)
+    f_numero = FieldDefn('NUMERO', OFTString)
+    f_part.SetWidth(8)
     f_dimensione = FieldDefn('DIMENSIONE', OFTInteger)
     f_angolo = FieldDefn('ANGOLO', OFTReal)
     f_pos_x = FieldDefn('POSIZIONEX', OFTReal)
@@ -39,8 +42,12 @@ def write_foglio(foglio, destination, format_name='ESRI Shapefile'):
     f_simbolo = FieldDefn('SIMBOLO', OFTInteger)
     f_etichetta = FieldDefn('etichetta', OFTString)
     f_etichetta.SetWidth(32)
+    f_testo = FieldDefn('TESTO', OFTString)
+    f_testo.SetWidth(256)
 
     ds = GetDriverByName(format_name).CreateDataSource(destination)
+
+    # tipo BORDO
     bordi = ds.CreateLayer('BORDI', gauss_boaga_ovest, wkbPolygon)
 
     bordi.CreateField(f_comune)
@@ -55,15 +62,15 @@ def write_foglio(foglio, destination, format_name='ESRI Shapefile'):
     bordi.CreateField(f_interno_y)
     bordi.CreateField(f_etichetta)
 
-    for bordo in foglio['oggetti']['BORDO']:
+    for oggetto in foglio['oggetti']['BORDO']:
         poly = Geometry(wkbPolygon)
-        tabisole = map(int, bordo['TABISOLE'])
+        tabisole = map(int, oggetto['TABISOLE'])
 
         # contorno esterno
-        vertici_contorno = int(bordo['NUMEROVERTICI']) - sum(tabisole)
+        vertici_contorno = int(oggetto['NUMEROVERTICI']) - sum(tabisole)
         ring = Geometry(wkbLinearRing)
         for vertice in range(vertici_contorno):
-            x, y = map(float, bordo['VERTICI'][vertice])
+            x, y = map(float, oggetto['VERTICI'][vertice])
             if True:
                 x, y = trasformation.TransformPoint(x, y)[:2]
             ring.AddPoint(x, y)
@@ -71,24 +78,24 @@ def write_foglio(foglio, destination, format_name='ESRI Shapefile'):
         poly.AddGeometry(ring)
 
         # isole
-        for isola in range(int(bordo['NUMEROISOLE'])):
+        for isola in range(int(oggetto['NUMEROISOLE'])):
             ring = Geometry(wkbLinearRing)
             for vertice in range(vertice + 1, vertice + 1 + tabisole[isola]):
-                x, y = map(float, bordo['VERTICI'][vertice])
+                x, y = map(float, oggetto['VERTICI'][vertice])
                 if True:
                     x, y = trasformation.TransformPoint(x, y)[:2]
                 ring.AddPoint(x, y)
             ring.CloseRings()
             poly.AddGeometry(ring)
 
-        etichetta = bordo['CODICE IDENTIFICATIVO']
-        if len(bordo['CODICE IDENTIFICATIVO']) == 11:
+        etichetta = oggetto['CODICE IDENTIFICATIVO']
+        if len(oggetto['CODICE IDENTIFICATIVO']) == 11:
             tipo = 'CONFINE'
-        elif bordo['CODICE IDENTIFICATIVO'] == 'STRADA':
+        elif oggetto['CODICE IDENTIFICATIVO'] == 'STRADA':
             tipo = 'STRADA'
-        elif bordo['CODICE IDENTIFICATIVO'] == 'ACQUA':
+        elif oggetto['CODICE IDENTIFICATIVO'] == 'ACQUA':
             tipo = 'ACQUA'
-        elif bordo['CODICE IDENTIFICATIVO'][-1] == '+':
+        elif oggetto['CODICE IDENTIFICATIVO'][-1] == '+':
             tipo = 'FABBRICATO'
             etichetta = ''
         else:
@@ -98,11 +105,11 @@ def write_foglio(foglio, destination, format_name='ESRI Shapefile'):
         feat.SetField('COMUNE', foglio['CODICE COMUNE'])
         feat.SetField('FOGLIO', foglio['CODICE FOGLIO'])
         feat.SetField('TIPO', tipo)
-        feat.SetField('PARTICELLA', bordo['CODICE IDENTIFICATIVO'])
-        feat.SetField('DIMENSIONE', int(bordo['DIMENSIONE']))
-        feat.SetField('ANGOLO', float(bordo['ANGOLO']))
-        pos_x, pos_y = map(float, (bordo['POSIZIONEX'], bordo['POSIZIONEY']))
-        interno_x, interno_y = map(float, (bordo['PUNTOINTERNOX'], bordo['PUNTOINTERNOY']))
+        feat.SetField('PARTICELLA', oggetto['CODICE IDENTIFICATIVO'])
+        feat.SetField('DIMENSIONE', int(oggetto['DIMENSIONE']))
+        feat.SetField('ANGOLO', float(oggetto['ANGOLO']))
+        pos_x, pos_y = map(float, (oggetto['POSIZIONEX'], oggetto['POSIZIONEY']))
+        interno_x, interno_y = map(float, (oggetto['PUNTOINTERNOX'],oggetto['PUNTOINTERNOY']))
         if True:
             pos_x, pos_y = trasformation.TransformPoint(pos_x, pos_y)[:2]
             interno_x, interno_y = trasformation.TransformPoint(interno_x, interno_y)[:2]
@@ -115,10 +122,63 @@ def write_foglio(foglio, destination, format_name='ESRI Shapefile'):
         bordi.CreateFeature(feat)
         feat.Destroy()
 
-    fiduciali = ds.CreateLayer('FIDUCIALI', gauss_boaga_ovest, wkbPoint)
 
-    f_numero = FieldDefn('numero', OFTString)
-    f_numero.SetWidth(8)
+    # tipo TESTO
+    testi = ds.CreateLayer('TESTI', gauss_boaga_ovest, wkbPoint)
+
+    testi.CreateField(f_comune)
+    testi.CreateField(f_foglio)
+    testi.CreateField(f_testo)
+    testi.CreateField(f_dimensione)
+    testi.CreateField(f_angolo)
+    testi.CreateField(f_etichetta)
+
+    for oggetto in foglio['oggetti']['TESTO']:
+        x, y = map(float, (oggetto['POSIZIONEX'], oggetto['POSIZIONEY']))
+        if True:
+            x, y = trasformation.TransformPoint(x, y)[:2]
+        # FIXME: many texts are useless, prun them from etichetta
+        etichetta = oggetto['TESTO']
+        
+        feat = Feature(testi.GetLayerDefn())
+        feat.SetField('COMUNE', foglio['CODICE COMUNE'])
+        feat.SetField('FOGLIO', foglio['CODICE FOGLIO'])
+        feat.SetField('TESTO', oggetto['TESTO'])
+        feat.SetField('DIMENSIONE', int(oggetto['DIMENSIONE']))
+        feat.SetField('ANGOLO', float(oggetto['ANGOLO']))
+        feat.SetField('etichetta', etichetta)
+        pt = Geometry(wkbPoint)
+        pt.SetPoint_2D(0, x, y)
+        feat.SetGeometry(pt)
+        testi.CreateFeature(feat)
+
+
+    # tipo SIMBOLO
+    simboli = ds.CreateLayer('SIMBOLI', gauss_boaga_ovest, wkbPoint)
+
+    simboli.CreateField(f_comune)
+    simboli.CreateField(f_foglio)
+    simboli.CreateField(f_simbolo)
+    simboli.CreateField(f_angolo)
+
+    for oggetto in foglio['oggetti']['SIMBOLO']:
+        x, y = map(float, (oggetto['POSIZIONEX'], oggetto['POSIZIONEY']))
+        if True:
+            x, y = trasformation.TransformPoint(x, y)[:2]
+        
+        feat = Feature(simboli.GetLayerDefn())
+        feat.SetField('COMUNE', foglio['CODICE COMUNE'])
+        feat.SetField('FOGLIO', foglio['CODICE FOGLIO'])
+        feat.SetField('SIMBOLO', oggetto['CODICE SIMBOLO'])
+        feat.SetField('ANGOLO', float(oggetto['ANGOLO']))
+        pt = Geometry(wkbPoint)
+        pt.SetPoint_2D(0, x, y)
+        feat.SetGeometry(pt)
+        simboli.CreateFeature(feat)
+
+
+    # tipo FIDUCIALE
+    fiduciali = ds.CreateLayer('FIDUCIALI', gauss_boaga_ovest, wkbPoint)
 
     fiduciali.CreateField(f_comune)
     fiduciali.CreateField(f_foglio)
@@ -128,20 +188,20 @@ def write_foglio(foglio, destination, format_name='ESRI Shapefile'):
     fiduciali.CreateField(f_pos_y)
     fiduciali.CreateField(f_etichetta)
 
-    for fiduciale in foglio['oggetti']['FIDUCIALE']:
-        x, y = map(float, (fiduciale['POSIZIONEX'], fiduciale['POSIZIONEY']))
-        pos_x, pos_y = map(float, (fiduciale['PUNTORAPPRESENTAZIONEX'], fiduciale['PUNTORAPPRESENTAZIONEY']))
+    for oggetto in foglio['oggetti']['FIDUCIALE']:
+        x, y = map(float, (oggetto['POSIZIONEX'], oggetto['POSIZIONEY']))
+        pos_x, pos_y = map(float, (oggetto['PUNTORAPPRESENTAZIONEX'], oggetto['PUNTORAPPRESENTAZIONEY']))
         if True:
             x, y = trasformation.TransformPoint(x, y)[:2]
             pos_x, pos_y = trasformation.TransformPoint(pos_x, pos_y)[:2]
-        etichetta = 'PF%02d/%s%s/%s' % (int(fiduciale['NUMERO IDENTIFICATIVO']),
+        etichetta = 'PF%02d/%s%s/%s' % (int(oggetto['NUMERO IDENTIFICATIVO']),
             foglio['CODICE NUMERO FOGLIO'][1:], foglio['CODICE ALLEGATO'], foglio['CODICE COMUNE'])
         
         feat = Feature(fiduciali.GetLayerDefn())
         feat.SetField('COMUNE', foglio['CODICE COMUNE'])
         feat.SetField('FOGLIO', foglio['CODICE FOGLIO'])
-        feat.SetField('NUMERO', fiduciale['NUMERO IDENTIFICATIVO'])
-        feat.SetField('SIMBOLO', fiduciale['CODICE SIMBOLO'])
+        feat.SetField('NUMERO', oggetto['NUMERO IDENTIFICATIVO'])
+        feat.SetField('SIMBOLO', oggetto['CODICE SIMBOLO'])
         feat.SetField('POSIZIONEX', pos_x)
         feat.SetField('POSIZIONEY', pos_y)
         feat.SetField('etichetta', etichetta)
@@ -149,3 +209,29 @@ def write_foglio(foglio, destination, format_name='ESRI Shapefile'):
         pt.SetPoint_2D(0, x, y)
         feat.SetGeometry(pt)
         fiduciali.CreateFeature(feat)
+
+
+    # tipo LINEA
+    linee = ds.CreateLayer('LINEE', gauss_boaga_ovest, wkbLineString)
+
+    linee.CreateField(f_comune)
+    linee.CreateField(f_foglio)
+    linee.CreateField(f_simbolo)
+
+    for oggetto in foglio['oggetti']['LINEA']:
+        # contorno esterno
+        vertici = int(oggetto['NUMEROVERTICI'])
+        linea = Geometry(wkbLineString)
+        for vertice in range(vertici):
+            x, y = map(float, oggetto['VERTICI'][vertice])
+            if True:
+                x, y = trasformation.TransformPoint(x, y)[:2]
+            linea.AddPoint(x, y)
+
+        feat = Feature(linee.GetLayerDefn())
+        feat.SetField('COMUNE', foglio['CODICE COMUNE'])
+        feat.SetField('FOGLIO', foglio['CODICE FOGLIO'])
+        feat.SetField('SIMBOLO', oggetto['CODICE TIPO DI TRATTO'])
+        feat.SetGeometry(linea)
+        linee.CreateFeature(feat)
+        feat.Destroy()
