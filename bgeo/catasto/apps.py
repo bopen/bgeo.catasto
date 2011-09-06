6163d29de12f9e10d6b8d48eca89aec4288ccac6
Alessandro Amici <a.amici@bopen.it>
2011-09-06 16:19:26 +0200

    new: add -P option and accept CXF filename in input

--- bgeo/catasto/apps.py
+++ bgeo/catasto/apps.py
@@ -12,15 +12,21 @@ def main_cxf(args=argv[1:]):
         help="destination datasource name")
     parser.add_option("-f", "--format-name", default='ESRI Shapefile',
         help="output file format name, see OGR docs for possible values")
+    parser.add_option("-P", "--point-borders", default=False, action="store_true",
+        help="add a duplicate BORDI layer with point features (useful for labeling)")
     (keys, args) = parser.parse_args(args=args)
     assert len(args) == 1
+
+    if args[0].endswith('.CXF'):
+        args[0] = args[0][:-4]
 
     foglio = parse_foglio(args[0])
     if keys.destination is not None:
         destination = keys.destination
     else:
         destination = args[0]
-    write_foglio(foglio, destination)
+    write_foglio(foglio, destination, point_borders=keys.point_borders,
+        format_name=keys.format_name)
 
 
 def main_ter(args=argv[1:]):
@@ -42,4 +48,4 @@ def main_ter(args=argv[1:]):
 
 
 if __name__ == '__main__':
-    main_ter()
+    main_cxf()
