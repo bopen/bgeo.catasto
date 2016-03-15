af1287eb0d50abe3edf7f9c12e14bde43a72eee1
Alessandro Amici <alexamici@gmail.com>
2016-03-15 14:57:31 +0100

    Sync package metadata for release.

---
+++ CHANGELOG.rst
@@ -0,0 +1,7 @@
+
+0.3.0 (unreleased)
+
+------------------
+
+- Add support for Python 3.
+- Drop stale special purpose code.
--- setup.py
+++ setup.py
@@ -12,12 +12,17 @@ setup(
         "Development Status :: 3 - Alpha",
         "Environment :: Console",
         "License :: OSI Approved :: GNU General Public License (GPL)",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         "Topic :: Scientific/Engineering :: GIS",
     ],
     keywords='',
     author='Alessandro Amici',
-    author_email='',
-    url='http://github.com/alexamici/bgeo.catasto',
+    author_email='a.amici@bopen.eu',
+    url='http://github.com/bopen/bgeo.catasto',
     license='GPL',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['bgeo'],
@@ -33,6 +38,5 @@ setup(
     entry_points="""
         [console_scripts]
         cxf2ogr = bgeo.catasto.apps:main_cxf
-        ter2db = bgeo.catasto.apps:main_ter
     """,
 )
